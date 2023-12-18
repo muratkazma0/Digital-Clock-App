274eb1bcb9794f7ed718c089443811cffee71210
Murat Kazma <muratkazma69@gmail.com>
2023-12-18 23:55:52 +0300

    Digital Clock App
    The Digital Clock project is a simple yet effective web application that displays the current time in a digital format. Developed using HTML, CSS, and JavaScript, this project offers a user-friendly interface with a clean design to showcase the current hour, minute, and second.

---
+++ Digital Clock App/app.js
@@ -0,0 +1,45 @@
+function update_Clock() {
+
+	const now = new Date();
+	const dayname = now.getDay(),
+		month = now.getMonth(),
+		date = now.getDate(),
+		year = now.getFullYear(),
+		hours = now.getHours(),
+		min = now.getMinutes(),
+		sec = now.getSeconds(),
+		period = "AM";
+
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "September" , "November", "December"]
+	const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+	if (hours == 0)
+		hours = 12;
+
+	if (hours > 12) {
+		// hours  = hours -12;
+		period = "PM";
+	}
+
+	// add 0 in the beginning of number if less than 10
+	hours = (hours < 10) ? "0" + hours : hours;
+	min = (min < 10) ? "0" + min : min;
+	sec = (sec < 10) ? '0' + sec : sec;
+
+	document.querySelector("#dayname").innerHTML = days[dayname];
+	document.querySelector("#month").innerHTML = months[month];
+	document.querySelector("#daynum").innerHTML = date;
+	document.querySelector("#year").innerHTML = year;
+	document.querySelector("#hour").innerHTML = hours;
+	document.querySelector("#minutes").innerHTML = min;
+	document.querySelector("#seconds").innerHTML = sec;
+	document.querySelector("#period").innerHTML = period;
+
+}
+
+function intiClock() {
+	update_Clock();
+	window.setInterval("update_Clock()", 1);
+}
+
+intiClock();
